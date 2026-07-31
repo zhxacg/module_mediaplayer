1d80cabf5f74f1b75fe783064a3ceacdadde1fa3
kalu <153437803@qq.com>
2026-07-31 18:15:37 +0800

    update file

--- lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/kernel/video/mediax/hls/CustomTag.java
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/kernel/video/mediax/hls/CustomTag.java
@@ -2,7 +2,7 @@ package lib.kalu.mediaplayer.core.kernel.video.mediax.hls;
 
 public enum CustomTag {
 
-    MASTER_PLAY_URL_VIDEO("MASTER_PLAY_URL_VIDEO")
+    MASTER_PLAY_URL_VIDEO("MASTER_PLAY_URL_VIDEO");
     private final String value;
 
     // 枚举构造方法（默认是 private 的）
